0b89ebe80dc163a98155a63fb2f4e29e7c39f68d
Ryan <97636691+rbass6@users.noreply.github.com>
2023-10-05 20:33:49 -0400

    Finish problem 1

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -11,34 +11,38 @@ import { withRouter } from "react-router";
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
-    this.getPath = this.getPath.bind(this);
     this.userContext = this.userContext.bind(this);
   }
 
   getPath(){
     const path = this.props.location.pathname.split("/")[1];
     return path;
-    // console.log(path);
+  }
+
+  getUser(){
+    const userId = this.props.location.pathname.split("/")[2];
+    return window.models.userModel(userId);
   }
 
   userContext(){
+    const user = this.getUser();
     if (this.getPath() === "photos") {
-      return "Photos";
+      return ("Photos of " + user.first_name + " " + user.last_name);
     } else if (this.getPath() === "users"){
-      return ("Users");
+      return (user.first_name + " " + user.last_name);
     } else {
-      return ("");
+      return ("Select a user");
     }
   }
 
   render() {
     return (
       <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
+        <Toolbar className="topbar-toolbar">
           <Typography variant="h5" color="inherit">
             ITCS 6112 Team Diversity&apos;s Photo App
           </Typography>
-          <Typography>
+          <Typography variant="h5">
             {this.userContext()}
           </Typography>
         </Toolbar>
